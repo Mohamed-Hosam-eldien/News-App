f4fcd5d2cc9d38b967b89b25d7533822f4d475c3
mohamedhosam <mohamedhosameldien07@gmail.com>
2022-03-25 09:24:33 +0200

    refactor code in login activity

--- app/src/main/java/com/example/news/login/LoginActivity.kt
+++ app/src/main/java/com/example/news/login/LoginActivity.kt
@@ -1,24 +1,24 @@
 package com.example.news.login
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.lifecycle.LifecycleOwner
+import androidx.appcompat.app.AlertDialog
+import com.example.news.MainActivity
 import com.example.news.R
 import com.example.news.databinding.ActivityLoginBinding
 import com.example.news.utlities.Utility
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 import java.util.*
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
 
-    private val loginViewModel:LoginViewModel by viewModels()
-    private lateinit var binding : ActivityLoginBinding
+    private val loginViewModel: LoginViewModel by viewModels()
+    private lateinit var binding: ActivityLoginBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,23 +29,19 @@ class LoginActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
         binding.viewmodel = loginViewModel
 
-
-
         binding.btnLogin.setOnClickListener {
 
-            if (isDataValidate()){
-                loginViewModel.getUserFromDataBase("doaa","000")
+            if (isDataValidate()) {
+                loginViewModel.getUserFromDataBase("doaa", "000")
                     .observe(this) {
-                        if(it!=null){
+                        if (it != null) {
 
-                          Utility.USER = it
-                            var intent = Intent(this, MainActivity::class.java)
+                            Utility.USER = it
+                            val intent = Intent(this, MainActivity::class.java)
                             startActivity(intent)
 
-                        }
-                        else{
-                            //Log.e(TAG, "onCreate:  else ", )
-                            Toast.makeText(this, "Not correct", Toast.LENGTH_SHORT).show()
+                        } else {
+                            showUserNotExistDialog()
                         }
                     }
             }
@@ -53,23 +49,30 @@ class LoginActivity : AppCompatActivity() {
         }
 
 
-
-
     }
 
-    fun isDataValidate() : Boolean{
-        var password = binding.edtPassword.text.toString()
-        var email = binding.edtEmail.text.toString()
-        if (!Utility.isPassValid(password) || password.isEmpty()){
+    fun isDataValidate(): Boolean {
+        val password = binding.edtPassword.text.toString()
+        val email = binding.edtEmail.text.toString()
+        if (!Utility.isPassValid(password) || password.isEmpty()) {
             binding.edtPassword.error = "Write correct password"
             return false
         }
 
-        if (!Utility.isEmailValid(email) || email.isEmpty()){
+        if (!Utility.isEmailValid(email) || email.isEmpty()) {
             binding.edtEmail.error = "Write correct email"
             return false
         }
         return true
+    }
+
+    private fun goToRegisterScreen() {
+        Log.d(TAG, "goToRegisterScreen: ")
+        // intent handle
+        finish()
+    }
+
+
     private fun showUserNotExistDialog() {
         val alertDialog = AlertDialog.Builder(this)
 
@@ -83,11 +86,5 @@ class LoginActivity : AppCompatActivity() {
         }.create().show()
     }
 
-    private fun goToRegisterScreen() {
-        Log.d(TAG, "goToRegisterScreen: ")
-        // intent handle
-        finish()
-    }
-    
-    
+
 }
