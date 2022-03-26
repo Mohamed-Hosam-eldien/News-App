55ee84eb8666065404b671e3440b39c19a9437ed
Doaa essam <doaaessam2021@gmail.com>
2022-03-26 16:05:50 +0200

    handle internet connection show data from database in home

--- app/src/main/java/com/example/news/models/Source.kt
+++ app/src/main/java/com/example/news/models/Source.kt
@@ -1,6 +1,6 @@
 package com.example.news.models
 
 data class Source(
-    val id: String,
-    val name: String
+    val id: String?,
+    val name: String?
 )
--- app/src/main/java/com/example/news/newsHome/NewsFragment.kt
+++ app/src/main/java/com/example/news/newsHome/NewsFragment.kt
@@ -1,15 +1,21 @@
 package com.example.news.newsHome
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.news.R
 import com.example.news.databinding.FragmentNewsBinding
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -34,21 +40,15 @@ class NewsFragment : Fragment() ,OnNewClickListener{
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
           setUpUi()
-        newsHomeViewModel.getAllNewsFromApi()
-        newsHomeViewModel.getNews.observe(viewLifecycleOwner
-        ) {
-            if(it!=null){
-                newsHomeAdapter.setData(it.articles)
-            }
-            else{
 
-            }
 
-        }
+
     }
 
     private fun setUpUi() {
+        newsHomeViewModel.getAllNewsForHome(requireContext())
 
+        obserData()
             binding.newsRecycle.apply {
                 setHasFixedSize(true)
                 layoutManager = LinearLayoutManager(requireContext(),
@@ -56,10 +56,105 @@ class NewsFragment : Fragment() ,OnNewClickListener{
                 adapter=newsHomeAdapter
             }
 
+
         }
+
+    private fun obserData() {
+        observeShowData()
+        observeLoading()
+        observeShowError()
+    }
+
+    private fun observeShowError() {
+        newsHomeViewModel.getError.observe(viewLifecycleOwner) {
+            it?.let {
+                showMessage(it)
+            }
+        }
+    }
+
+    private fun showMessage(it: String) {
+            Snackbar.make(requireView(), it, Snackbar.LENGTH_INDEFINITE)
+                .setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE).setBackgroundTint(
+                    resources.getColor(
+                        R.color.black
+                    )
+                )
+                .setActionTextColor(resources.getColor(R.color.white)).setAction("قفل")
+                {
+                }.show()
+        }
+
+    private fun observeLoading() {
+        newsHomeViewModel.getLoad.observe(viewLifecycleOwner) {
+            it?.let {
+                //binding.progress.visibility = it
+            }
+        }
+    }
+
+    private fun observeShowData() {
+        newsHomeViewModel.getNews.observe(viewLifecycleOwner
+        ) {
+            if (it != null) {
+                newsHomeAdapter.setData(it)
+            } else {
+
+            }
+        }
+    }
 
 
     override fun OnClick() {
 
     }
+
+
+    private fun registerConnectivityNetworkMonitor() {
+        if (requireContext() != null) {
+            val connectivityManager = requireContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val builder = NetworkRequest.Builder()
+            connectivityManager.registerNetworkCallback(builder.build(),
+                object : ConnectivityManager.NetworkCallback() {
+                    override fun onAvailable(network: Network) {
+                        super.onAvailable(network)
+                        if (activity != null) {
+                            activity!!.runOnUiThread {
+                                newsHomeViewModel.getAllNewsForHome(requireContext())
+
+                                //                                binding.textNoInternet.visibility = View.GONE
+//                                binding.noNetworkResult.visibility = View.GONE
+//                                binding.linearLayout.visibility = View.VISIBLE
+//                                viewModel.getServicesData()
+                            }
+                        }
+                    }
+
+                    override fun onLost(network: Network) {
+                        super.onLost(network)
+                        if (activity != null) {
+                            activity!!.runOnUiThread {
+                                Toast.makeText(
+                                    context, "لا يوجد انترنت ",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    }
+
+//    private fun checkNetwork(){
+//        if (NetworkConnection.checkInternetConnection(requireContext())) {
+//            viewModel.getServicesData()
+//        } else {
+//            binding.textNoInternet.visibility = View.VISIBLE
+//            binding.noNetworkResult.visibility = View.VISIBLE
+//            binding.linearLayout.visibility = View.GONE
+//
+//        }
+//    }
 }
